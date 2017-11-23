445e847d4f8c098fc42d4497ce2671b3675b0d32
IcusFMagni <danika.ragnhild@gmail.com>
2017-11-23 13:12:50 -0600

    creates a full row of task!

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -15,11 +15,19 @@ function getAllToDo() {
             $('#shoeList').empty();
             for (let i = 0; i < response.length; i++) {
                 var todo = response[i];
-                var $newTodo = $('<tr><td>f</td></tr>');
+                var $newTodo = $('<tr><td>' + todo.due + '</td><td>' + todo.task + '</td><td>' + todo.steps + '</td><td>' + todo.completed + '</td></tr>');
 
-                var $completeButton = $('<td><button class=complete>Complete</button></td>')
+                var $deleteButton = $('<td><button class="delete">Delete</button></td>')
+                $deleteButton.data('id', todo.id);
+                $newTodo.prepend($deleteButton)
+
+                var $completeButton = $('<td><button class="complete">Complete</button></td>')
                 $completeButton.data('id', todo.id);
-                $newTodo.append($completeButton);
+                $newTodo.prepend($completeButton);
+
+
+
+
                 $('#toDoList').append($newTodo)
                 // var shoe=
                 // var $newShoeItem = $('<li>' + todo.task + '</li>');
