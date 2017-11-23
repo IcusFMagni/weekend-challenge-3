9ece04d51ac86566c3e1d74ef388c4faa8c3853d
IcusFMagni <danika.ragnhild@gmail.com>
2017-11-23 12:18:17 -0600

    Installed pg (cause I forgot initially) added pool.js)

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ var port = 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('server/public'));
 
+
+
 app.listen(port, function () {
     console.log('server is listening on port', port);
 });
